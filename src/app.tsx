10033a53f51c08fafa8c5748a187f10f2afab976
Cstylefly <52144668+Cstylefly@users.noreply.github.com>
2023-04-23 14:25:10 +0800

    feat(layout):添加登录重定向方法

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,10 @@
-import { WaterMark } from '@ant-design/pro-layout';
+import { history } from 'umi';
 import styles from './app.less';
 type CurrentUser = {
   type: string;
 };
+
+const loginPath = '/login' // 登陆页路由
 
 export async function getInitialState(): Promise<{
   currentUser: CurrentUser;
@@ -12,7 +14,7 @@ export async function getInitialState(): Promise<{
   };
 }
 
-export async function layout() {
+export async function layout({initialState,setInitialState}:{initialState:any,setInitialState:any}) {
   return {
     menuDataRender: () => [
       {
@@ -35,5 +37,33 @@ export async function layout() {
     },
     footerRender: false,
     pageTitleRender:false, //修改浏览器左侧项目名显示样式为title而不是 title-pageName
+    //rightContentRender：用于渲染头部右侧的节点，如用户头像，用户中心等。
+    onPageChange:(location:{pathname?:string | undefined}) => {
+      // if(!initialState.currentUser?.userId){
+      //   redirectLogin()
+      // }
+    }
   };
+}
+
+//重定向到登陆页
+const redirectLogin = () => {
+  const {pathname,query} = history.location
+  if(pathname === loginPath){
+    history.replace({
+      pathname:loginPath,
+      query:{
+        ...query
+      }
+    })
+  }else{
+    history.replace({
+      pathname:loginPath,
+      query:{
+        redirect:query?.redirect || pathname,
+        ...query
+      }
+    })
+  }
+
 }
