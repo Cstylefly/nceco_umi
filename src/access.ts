2f52db53cb3a8cd953960f9d2d1d38efd21dafce
Cstylefly <52144668+Cstylefly@users.noreply.github.com>
2023-04-23 17:53:32 +0800

    feat:清空console

--- src/access.ts
+++ src/access.ts
@@ -1,5 +1,4 @@
 export default function(initialState:{currentUser:{type:string}}){
-    console.log(initialState,'----initialState---');
     const {currentUser} = initialState;
     return {
         routePermission:true
