4526842d74c80deb8f8ad5bc251e3dd241f4fc62
Cstylefly <52144668+Cstylefly@users.noreply.github.com>
2023-04-26 17:29:58 +0800

    fix:修改.d.ts命名空间不识别问题

--- src/api/login/login.ts
+++ src/api/login/login.ts
@@ -1,6 +1,6 @@
 import { get,post,del,ResponseType } from '@/utils/request/request'
 
 //模拟登录接口
-export const login = (data:APP.LoginItemType) => {
+export const login = (data:API.LoginItemType) => {
     return post<ResponseType<API.LoginUserInfoType>>('/api/login', data)
 }
